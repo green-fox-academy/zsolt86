c878acbe4e8f7008bf13870a082c11c02413ec06
Lengyel Zsolt <lengyelzsolt@outlook.com>
2020-03-31 11:50:03 +0200

    Carrier started with ArrayList

--- week04/day01/src/aircraftcarrier/Carrier.java
+++ week04/day01/src/aircraftcarrier/Carrier.java
@@ -1,4 +1,23 @@
 package aircraftcarrier;
 
+import java.util.ArrayList;
+
 public class Carrier {
+  int ammoCarrier;
+  int hpCarrier;
+  ArrayList<Aircraft> carrier;
+
+  public Carrier(int ammoCarrier, int hpCarrier) {
+    this.ammoCarrier = ammoCarrier;
+    this.hpCarrier = hpCarrier;
+    this.carrier = new ArrayList<Aircraft>();
+  }
+
+  public void add(Aircraft aircraft) {
+    carrier.add(aircraft);
+  }
+
+  public void fill() {
+  }
+
 }
