1df512d57c2583236103a288417133d0065ec18c
Lengyel Zsolt <lengyelzsolt@outlook.com>
2020-03-31 07:59:52 +0200

    Aircrafts finished

---
+++ week04/day01/src/aircraftcarrier/F16.java
@@ -0,0 +1,12 @@
+package aircraftcarrier;
+
+public class F16 extends Aircraft {
+
+  public F16() {
+    super();
+    ammoCapacity = 8;
+    baseDamage = 30;
+    type = "F–16 Fighting Falcon";
+    isPriority = false;
+  }
+}
---
+++ week04/day01/src/aircraftcarrier/F35.java
@@ -0,0 +1,12 @@
+package aircraftcarrier;
+
+public class F35 extends Aircraft {
+
+  public F35() {
+    super();
+    ammoCapacity = 12;
+    baseDamage = 50;
+    type = "F–35 Lightning II";
+    isPriority = true;
+  }
+}
